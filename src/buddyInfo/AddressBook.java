1f44e59d889779e6a40aa415c914f5888af727e8
aashnanarang <aashnanarang@cb5109-33.labs.sce.carleton.ca>
2019-09-23 12:04:43 -0400

    Add basic functionality

---
+++ src/buddyInfo/AddressBook.java
@@ -0,0 +1,32 @@
+package buddyInfo;
+
+import java.util.*;
+
+public class AddressBook {
+
+	private List<BuddyInfo> buddyInfo;
+	
+	public AddressBook() {
+		this.buddyInfo = new ArrayList<BuddyInfo>();
+	}
+	
+	public void addBuddy(BuddyInfo bud) {
+		if (bud != null) {
+			this.buddyInfo.add(bud);
+		}
+	}
+	
+	public BuddyInfo removeBuddy(int index) {
+		if (index >= 0 && index < this.buddyInfo.size()) {
+			return this.buddyInfo.remove(index);
+		}
+		return null;
+	}
+	
+	public static void main(String args[]) {
+		BuddyInfo buddy = new BuddyInfo("Dani", "deerfox drive", "613-234-3333");
+		AddressBook addy = new AddressBook(); 
+		addy.addBuddy(buddy);
+		addy.removeBuddy(0);
+	}
+}
